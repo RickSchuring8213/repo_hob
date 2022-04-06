a51b8aad047ce05e58e1e743b876704cbc06b285
RickSchuring8213 <r.schuring@outlook.com>
2022-04-06 20:13:42 +0200

    Tussentijds opslaan

--- webscraper_project/1_scraping_data/data_scraper_indeed.py
+++ webscraper_project/1_scraping_data/data_scraper_indeed.py
@@ -1,7 +1,24 @@
+from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
+import numpy as np
+import requests
 
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome('webscraper_project/1_scraping_data/chromedriver')
-print('Ok')
-driver.get('https://www.google.nl')
+search_title = 'data business intelligence'
+page_start = 0
+page_end = 0
+interval = 10
+
+df = pd.DataFrame(columns=['Title']) #, 'location', 'company', 'salary', 'rating', 'job_description'])
+for i in range(page_start, page_end + interval, interval):
+    url = f"https://nl.indeed.com/vacatures?q={search_title.replace(' ', '%20')}&radius=0&start={str(i)}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for x in soup.find_all('div', attrs = {'class': ' row result'}):
+        job_title = x.find('a', attrs = {'data-tn-element':'jobTitle'}).text
+       
+ 
+
+        df.append({'job_title': job_title}, ignore_index=True)
+
+    print("Output:",df.shape)
+df.to_csv('output.csv')
